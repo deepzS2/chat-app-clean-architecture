95053ff55d1acad005cd855686429b0193ea055a
deepzS2 <alanbackman10@hotmail.com>
2022-09-09 10:24:08 -0300

    ✨: Users repository

---
+++ src/repositories/in-memory/in-memory-users-repository.ts
@@ -0,0 +1,42 @@
+import { User } from '@entities/user'
+import { UsersRepository } from '@repositories/users-repository'
+
+export class InMemoryUsersRepository implements UsersRepository {
+	public users: User[] = []
+
+	async create(user: User): Promise<void> {
+		this.users.push(user)
+	}
+
+	async findByEmail(email: string): Promise<User | null> {
+		const user = this.users.find((user) => user.email === email)
+
+		if (!user) return null
+
+		return user
+	}
+
+	async findById(id: string): Promise<User | null> {
+		const user = this.users.find((user) => user.id === id)
+
+		if (!user) return null
+
+		return user
+	}
+
+	async update(id: string, user: Partial<User>): Promise<void> {
+		const userIndex = this.users.findIndex((u) => u.id === id)
+
+		if (userIndex === -1) throw new Error('User not found...')
+
+		this.users[userIndex] = Object.assign(this.users[userIndex], user)
+	}
+
+	async delete(id: string): Promise<void> {
+		const userIndex = this.users.findIndex((u) => u.id === id)
+
+		if (userIndex === -1) throw new Error('User not found...')
+
+		this.users.splice(userIndex, 1)
+	}
+}
---
+++ src/repositories/users-repository.ts
@@ -0,0 +1,9 @@
+import { User } from '@entities/user'
+
+export interface UsersRepository {
+	create(user: User): Promise<void>
+	findById(id: string): Promise<User | null>
+	findByEmail(email: string): Promise<User | null>
+	update(id: string, user: Partial<User>): Promise<void>
+	delete(id: string): Promise<void>
+}
