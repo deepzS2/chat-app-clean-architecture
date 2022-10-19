5991a955b5ee8d5ba9b9a85e9863011b60e9ae06
deepzS2 <alanbackman10@hotmail.com>
2022-10-19 11:27:19 -0300

    ✨(frontend): Query client

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -2,15 +2,20 @@ import { AppProps } from 'next/app'
 import { IoProvider } from 'socket.io-react-hook'
 
 import { AuthenticationProvider } from '@contexts/authentication.context'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 import '@styles/globals.css'
 
 export default function App({ Component, pageProps }: AppProps) {
+	const queryClient = new QueryClient()
+
 	return (
-		<AuthenticationProvider>
-			<IoProvider>
-				<Component {...pageProps} />
-			</IoProvider>
-		</AuthenticationProvider>
+		<QueryClientProvider client={queryClient}>
+			<AuthenticationProvider>
+				<IoProvider>
+					<Component {...pageProps} />
+				</IoProvider>
+			</AuthenticationProvider>
+		</QueryClientProvider>
 	)
 }
