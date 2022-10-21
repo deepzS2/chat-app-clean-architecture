1079740bcf87b56986fa8e738d4be9f38d7b813e
deepzS2 <alanbackman10@hotmail.com>
2022-10-21 09:43:12 -0300

    ✨(backend): Send message when user join

--- backend/src/app/events/connection.ts
+++ backend/src/app/events/connection.ts
@@ -1,7 +1,17 @@
 import { Server, Socket } from 'socket.io'
 
-export const onConnection = async (io: Server, socket: Socket) => {
-	socket.broadcast.emit('USER_JOINED', {
+import { Message } from '@entities/message'
+import { MessageDTO } from '@use-cases/messages/dto/message-dto'
+
+export const onConnection = (io: Server, socket: Socket) => {
+	const messageResult = Message.create({
 		content: `${socket.user.username} joined the chat!`,
+		authorId: socket.user.id,
 	})
+
+	if (messageResult.isSuccess) {
+		const message = messageResult.getValue()
+
+		socket.broadcast.emit('USER_JOINED', MessageDTO.fromEntity(message))
+	}
 }
