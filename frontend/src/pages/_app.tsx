4d105f5889f2d14c2d81e69f279fad77989d7c55
deepzS2 <alanbackman10@hotmail.com>
2022-10-21 09:44:24 -0300

    ✨(frontend): React-query config and removing socket.io context

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,5 +1,4 @@
 import { AppProps } from 'next/app'
-import { IoProvider } from 'socket.io-react-hook'
 
 import { AuthenticationProvider } from '@contexts/authentication.context'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
@@ -7,14 +6,18 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import '@styles/globals.css'
 
 export default function App({ Component, pageProps }: AppProps) {
-	const queryClient = new QueryClient()
+	const queryClient = new QueryClient({
+		defaultOptions: {
+			queries: {
+				staleTime: Infinity,
+			},
+		},
+	})
 
 	return (
 		<QueryClientProvider client={queryClient}>
 			<AuthenticationProvider>
-				<IoProvider>
-					<Component {...pageProps} />
-				</IoProvider>
+				<Component {...pageProps} />
 			</AuthenticationProvider>
 		</QueryClientProvider>
 	)
