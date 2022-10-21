5adc62ebfd57dee45a93d05f8e6b01c74b8b4f06
deepzS2 <alanbackman10@hotmail.com>
2022-10-21 09:43:45 -0300

    🐛(frontend): Fixing bug with react-hook-form

--- frontend/src/components/Input.tsx
+++ frontend/src/components/Input.tsx
@@ -1,15 +1,18 @@
-import { HTMLAttributes, InputHTMLAttributes } from 'react'
+import { HTMLAttributes, InputHTMLAttributes, forwardRef } from 'react'
 
 type Props = HTMLAttributes<HTMLInputElement> &
 	InputHTMLAttributes<HTMLInputElement>
 
-const Input = (props: Props) => {
+const Input = forwardRef<HTMLInputElement, Props>((props, ref) => {
 	return (
 		<input
+			ref={ref}
 			className="w-full font-poppins text-neutral-900 bg-neutral-100 border-[1px] border-neutral-200 px-4 py-2 text-base"
 			{...props}
 		/>
 	)
-}
+})
+
+Input.displayName = 'Input'
 
 export default Input
